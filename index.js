dc2fcb22bf766921f9e18feef65e01aa85e3df06
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2023-11-23 17:09:15 +0530

    complete project

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-function inputvalues(){
-       let value = document.getElementsByTagName('input').value;
-    console.log(value)
+const inputEl = document.getElementById("name");
+const inputE2 = document.getElementById("price");
+const inputBtn = document.getElementById("btn")
+function showvalues() {
+  const inputValue = parseFloat(inputEl.value);
+  const inputValue2 = parseFloat(inputE2.value);
+
+if(inputValue.length === 0 || inputValue2.length === 0){
+   alert("all fields are required");
+    return
+}
+    // Log the value to the console
+    
+        var tipAmount = (inputValue* inputValue2)/100;
+        var total = inputValue + tipAmount;
+        console.log(total)
+        document.getElementById("total").innerText=total.toFixed(2);
+inputEl.value=""
+inputE2.value=""
 }
