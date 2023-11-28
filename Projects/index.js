cfb4d0b78748353a1404b68647954bd560edcb83
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2023-11-28 15:47:26 +0530

    some modifications

--- Projects/index.js
+++ Projects/index.js
@@ -4,9 +4,13 @@ const currentYear = Year.getFullYear();
 function calculateAge(){
 const SelectedYear = new Date(dateOfBirth.value) // for extracting year from selected year
 const PersonYear=SelectedYear.getFullYear();
- const currentAge = currentYear-PersonYear;
-  console.log(currentAge);
-  const Age = document.getElementById("age");
-  Age.innerText = currentAge;
-  
+if(PersonYear>currentYear){
+  alert("pls select age between current year")
+}
+else{
+  const currentAge = currentYear-PersonYear;
+   console.log(currentAge);
+   const Age = document.getElementById("age");
+   Age.innerText = currentAge;
+}
 }
