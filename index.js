893c7e3d28e3c04c47e506def6447f118d859fa5
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2023-12-02 14:00:54 +0530

    list  of dynamic cards
    this is api generated list of dynamic data.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const inputEl = document.getElementById("name");
-const inputE2 = document.getElementById("price");
-const inputBtn = document.getElementById("btn")
-function showvalues() {
-  const inputValue = parseFloat(inputEl.value);
-  const inputValue2 = parseFloat(inputE2.value);
+document.addEventListener('DOMContentLoaded', () => {
+    const url = 'https://api.sampleapis.com/fakebank/accounts/';
 
-if(inputValue.length === 0 || inputValue2.length === 0){
-   alert("all fields are required");
-    return
-}
-    // Log the value to the console
-    
-        var tipAmount = (inputValue* inputValue2)/100;
-        var total = inputValue + tipAmount;
-        console.log(total)
-        document.getElementById("total").innerText=total.toFixed(2);
-inputEl.value=""
-inputE2.value=""
-}
+    fetch(url, { method: 'GET' })
+        .then((res) => res.json())
+        .then((data) => {
+            var lists = data.slice(0,8)
+            const container = document.querySelector(".cardInv"); // Change this if you want to append cards to a specific container
+     
+            lists.forEach((item) => {
+                // Create HTML content for the card
+                const cardHTML = `
+                    <div class="card" id="card_${item.id}">
+                        <p>Category: ${item.category}</p>
+                        <p>Debit: ${item.debit ? item.debit : 'N/A'}</p>
+                        <p>Description: ${item.description}</p>
+                        <p>Transaction Date: ${item.transactionDate}</p>
+                    </div>
+                `;
+
+                // Append the HTML content to the container
+                container.innerHTML += cardHTML;
+            });
+        })
+        .catch((err) => console.log(err));
+});
