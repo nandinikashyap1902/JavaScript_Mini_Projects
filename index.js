3fdd6afc693bbb9e69ade60e823c0dd256d39400
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2023-12-05 11:56:59 +0530

    Dice roll simulator

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const url = 'https://api.sampleapis.com/fakebank/accounts/';
+//const randomNumber = Math.floor(Math.random()*6)
+//console.log(randomNumber)
+// const arrayOfNumbers=[];
+// arrayOfNumbers.push(randomNumber);
+// for(let i in arrayOfNumbers){
+//     console.log(arrayOfNumbers[i])
+// }
+function diceNumbers(){
+    const randomNumber = Math.floor(Math.random()*6);
+    document.querySelector(".number").innerText = randomNumber;
+    const diceEl = document.querySelector(".dice");
+    diceEl.classList.add("roll-animation");
+    setTimeout(() => {
+      diceEl.classList.remove("roll-animation");
+      rollDice();
+    }, 1000);
+    var numberBox = document.createElement('div')
+    numberBox.className ="numberContainer"
+    numberBox.innerHTML = randomNumber;
+    //numberBox.appendChild("h1").innerText = "Roll"
+    document.body.appendChild(numberBox)
+    const diceFace = getDiceFace(rollResult);
+  diceEl.innerHTML = diceFace;
+  historyList.push(rollResult);
+}
 
-    fetch(url, { method: 'GET' })
-        .then((res) => res.json())
-        .then((data) => {
-            var lists = data.slice(0,8)
-            const container = document.querySelector(".cardInv"); // Change this if you want to append cards to a specific container
-     
-            lists.forEach((item) => {
-                // Create HTML content for the card
-                const cardHTML = `
-                    <div class="card" id="card_${item.id}">
-                        <p>Category: ${item.category}</p>
-                        <p>Debit: ${item.debit ? item.debit : 'N/A'}</p>
-                        <p>Description: ${item.description}</p>
-                        <p>Transaction Date: ${item.transactionDate}</p>
-                    </div>
-                `;
-
-                // Append the HTML content to the container
-                container.innerHTML += cardHTML;
-            });
-        })
-        .catch((err) => console.log(err));
-});
+function getDiceFace(rollResult) {
+    switch (rollResult) {
+      case 1:
+        return "&#9856;";
+      case 2:
+        return "&#9857;";
+      case 3:
+        return "&#9858;";
+      case 4:
+        return "&#9859;";
+      case 5:
+        return "&#9860;";
+      case 6:
+        return "&#9861;";
+      default:
+        return "";
+    }
+  }
+  
