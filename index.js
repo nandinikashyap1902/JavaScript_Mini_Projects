f7e5a2c17f3a0086fcf7792151b7ec7e0ae0a718
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2023-12-06 12:41:46 +0530

    updated roll dice

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-//const randomNumber = Math.floor(Math.random()*6)
-//console.log(randomNumber)
-// const arrayOfNumbers=[];
-// arrayOfNumbers.push(randomNumber);
-// for(let i in arrayOfNumbers){
-//     console.log(arrayOfNumbers[i])
-// }
 function diceNumbers(){
-    const randomNumber = Math.floor(Math.random()*6);
-    document.querySelector(".number").innerText = randomNumber;
-    const diceEl = document.querySelector(".dice");
+    
+    // document.querySelector(".number").innerText = randomNumber;
+     const diceEl = document.querySelector(".dice");
     diceEl.classList.add("roll-animation");
     setTimeout(() => {
       diceEl.classList.remove("roll-animation");
       rollDice();
     }, 1000);
-    var numberBox = document.createElement('div')
-    numberBox.className ="numberContainer"
-    numberBox.innerHTML = randomNumber;
-    //numberBox.appendChild("h1").innerText = "Roll"
-    document.body.appendChild(numberBox)
-    const diceFace = getDiceFace(rollResult);
-  diceEl.innerHTML = diceFace;
-  historyList.push(rollResult);
+  //   var numberBox = document.createElement('div')
+  //   numberBox.className ="numberContainer"
+  //   numberBox.innerHTML = randomNumber;
+  //   //numberBox.appendChild("h1").innerText = "Roll"
+  //   document.body.appendChild(numberBox)
+  //   const diceFace = getDiceFace(rollResult);
+  // diceEl.innerHTML = diceFace;
+  // historyList.push(rollResult);
+}
+let dicehistory=[];
+const rollHistory = document.querySelector(".diceNumers-Array");
+function rollDice(){
+ const randomNumber = Math.floor(Math.random()*6)+1;
+ const diceFace = getDiceFace(randomNumber);
+ const diceEl = document.querySelector(".dice");
+ diceEl.innerHTML = diceFace;
+ dicehistory.push(randomNumber)
+ updateDiceHistory();
 }
 
+function updateDiceHistory(){
+  rollHistory.innerHTML = "";
+  for(let i=0;i<dicehistory.length;i++){
+    const listItems = document.createElement("li");
+    listItems.innerHTML =`Roll ${i+1}: <span>${getDiceFace(dicehistory[i])}</span>`
+    rollHistory.appendChild(listItems);
+  }
+}
 function getDiceFace(rollResult) {
     switch (rollResult) {
       case 1:
